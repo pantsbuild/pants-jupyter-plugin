731f55935f5e53498fca4f9f4c1c8f9ac0e0774c
John Sirois <john.sirois@gmail.com>
2021-03-03 16:09:10 -0800

    Setup basic project tooling. (#14)
    Add:
    + pyproject.toml: For publishing and tool configuration.
    + tox.ini: For project automation. Standard formatting, tests and publishing for now.
    + tests/: With a simple seed test of the %pex_load magic
    
    Work towards #2 and #3.

---
+++ tests/test_plugin.py
@@ -0,0 +1,56 @@
+import subprocess
+from textwrap import dedent
+
+
+def test_pex_load(tmpdir) -> None:
+    pex_file = tmpdir / "colors.pex"
+    subprocess.run(["pex", "ansicolors==1.1.8", "-o", pex_file], check=True)
+    subprocess.run(
+        [
+            "ipython",
+            "-c",
+            dedent(
+                f"""\
+                try:
+                    import colors
+                    raise AssertionError(
+                        "Should not have been able to import colors before loading {pex_file}."
+                    )
+                except ImportError:
+                    # Expected.
+                    pass
+
+                %load_ext pants_jupyter_plugin
+                %pex_load {pex_file}
+                import colors
+                """
+            ),
+        ],
+        check=True,
+    )
+
+
+def test_requirements_load() -> None:
+    subprocess.run(
+        [
+            "ipython",
+            "-c",
+            dedent(
+                f"""\
+                try:
+                    import colors
+                    raise AssertionError(
+                        "Should not have been able to import colors before loading requirements."
+                    )
+                except ImportError:
+                    # Expected.
+                    pass
+
+                %load_ext pants_jupyter_plugin
+                %requirements_load "ansicolors==1.1.8"
+                import colors
+                """
+            ),
+        ],
+        check=True,
+    )
